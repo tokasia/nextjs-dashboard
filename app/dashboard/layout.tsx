79299c784aa79e30818f66cf1eba68c13e38ee3b
toka <toka@mindwareworks.com>
2024-07-09 11:19:36 +0900

    first

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+
+export default function Layout({ children }: { children: React.ReactNode}) {
+    return (
+        <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
+            <div className='w-full flex-node md:w-64'>
+                <SideNav />
+            </div>
+            <div className='flex-grow'>
+                {children}
+            </div>
+        </div>
+    )
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,23 @@
+import { Inter, Lusitana, Foldit, Nabla, Nanum_Brush_Script } from 'next/font/google'
+
+export const inter = Inter({subsets: ['latin']})
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
+
+export const foldit = Foldit({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
+
+export const nabla = Nabla({
+  weight: ['400'],
+  subsets: ['latin']
+})
+
+export const nanum_brush_script = Nanum_Brush_Script({
+    weight: ['400'],
+    subsets: ['latin']
+})
