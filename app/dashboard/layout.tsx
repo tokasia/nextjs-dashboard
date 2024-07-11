7b38e1fb7cf59077811a932ff0d0966c48613e2f
toka <toka@mindwareworks.com>
2024-07-11 12:24:39 +0900

    챕터10

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,15 +1,16 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 
+export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode}) {
-    return (
-        <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
-            <div className='w-full flex-node md:w-64'>
-                <SideNav />
-            </div>
-            <div className='flex-grow'>
-                {children}
-            </div>
-        </div>
-    )
+  return (
+    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
+      <div className='w-full flex-node md:w-64'>
+        <SideNav />
+      </div>
+      <div className='flex-grow'>
+        {children}
+      </div>
+    </div>
+  )
 }
