4999d349143c265fe8b20e990ce8d4fa998a0c09
toka <toka@mindwareworks.com>
2024-07-18 16:36:39 +0900

    finish

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+
 export default function Page() {
     return <h1>Customers Page</h1>
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,7 +15,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 30));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -158,6 +158,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
